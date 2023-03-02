0f6b4a564861c1de635dd149fe91779030bf16ad
sabri garrach <sabrigarrach10@gmail.com>
2023-03-02 19:22:02 +0100

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit ,  Renderer2} from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    Location, {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    }
+  ]
 })
 export class AppComponent {
   title = 'hotels';
